f9187d7d5cdaabc522e782e05f671151d4b78169
Franky W <frankywahl@users.noreply.github.com>
2021-10-01 21:37:46 +0200

    Consolidate helping commands

--- cmd/kms/kms.go
+++ cmd/kms/kms.go
@@ -7,6 +7,25 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	// ProgName is the name of this program.
+	ProgName = "biscuit"
+
+	// AliasPrefix is the prefix of all KMS Key Aliases.
+	AliasPrefix = "alias/" + ProgName + "-"
+
+	// GrantPrefix is the prefix of all KMS Grant Names.
+	GrantPrefix = ProgName + "-"
+)
+
+func kmsAliasName(label string) string {
+	return AliasPrefix + label
+}
+
+func cfStackName(label string) string {
+	return fmt.Sprintf("%s-%s", ProgName, label)
+}
+
 type regionError struct {
 	Region string
 	Err    error
